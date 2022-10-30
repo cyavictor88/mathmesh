0287dcf388f02e6ce2a1746e83a70de9e0d932f4
cyavictor88 <cyavictor88@gmail.com>
2022-10-30 17:25:53 +0700

    change fraction rect to use counterclockwise trianlges

--- src/mathml2mesh.ts
+++ src/mathml2mesh.ts
@@ -72,7 +72,8 @@ export class MathMlStringMesh {
                 char: '-',
                 uni: key,
                 verts: [xstart, ystart, 0, xstart, ystart + ylen, 0, xstart+xlen, ystart + ylen, 0, xstart+xlen, ystart, 0],
-                tris: [0, 1, 2, 3, 0, 2],
+                // tris: [0, 1, 2, 3, 0, 2], //clockwise
+                tris: [0, 2, 1, 3, 2, 0], //counter-clockwise
                 bbox: [0, 0, 0, 0]
             };
             this.jsonMeshes.push(newmesh);
